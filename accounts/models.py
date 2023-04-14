45e19cdfef8813dbda526bef7f8f29b19b62cfde
YurijBielousovDnepr <gakayur@gmail.com>
2023-04-14 23:20:49 +0300

    14-04 2 добавил модели

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,20 @@
+# models.py
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import User
+from art_event.models import Favorites
+from helpy.models import Region, Language
 
-# Create your models here.
+class Visitor(models.Model):
+    userNick = models.CharField(max_length=255)
+    category = models.CharField(max_length=255, choices=[('Helper', 'Helper'), ('Customer', 'Customer')], default='Customer')
+    district = models.ManyToManyField(Region)
+    languages = models.ManyToManyField(Language)
+    is_sponsor = models.BooleanField(default=False)
+    phone_number = models.CharField(max_length=20, blank=True)
+    email = models.EmailField(blank=True)
+    favorites = models.ForeignKey(Favorites, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.userNick
